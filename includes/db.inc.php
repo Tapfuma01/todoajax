5830e09d9833fd5526f930f02bb2f7c6f360a43f
Tapfuma <enosmhundwa@gmail.com>
2019-10-10 13:53:51 +0200

    update

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,14 +1,14 @@
 
 <?php
-
 $servername = "167.71.74.228";
 $username = "tapfuma";
 $password = "Impunity86Eyepiece12carbide";
+$database = "2019BWebIntensive_tapfuma";
 try {
    $conn = new PDO("mysql:host=$servername;port=3306;dbname=2019BWebIntensive_tapfuma", $username, $password);
    // set the PDO error mode to exception
    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   echo "Connected successfully";
+   echo "";
    }
 catch(PDOException $e)
    {
