8b875f2ae441eddc036447f7c622d1d2cd352682
Tapfuma <enosmhundwa@gmail.com>
2019-10-10 13:18:33 +0200

    Update db.inc.php

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -1,15 +1,17 @@
+
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "todo";
 
-	try {
-		$conn = new PDO('mysql:host='.$servername.'; dbname='.$database,$username, $password);
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	} catch(PDOException $e) {
-		echo("MySQL Error! Please contact the system admin.");
-		exit();
-	}
-
+$servername = "167.71.74.228";
+$username = "tapfuma";
+$password = "Impunity86Eyepiece12carbide";
+try {
+   $conn = new PDO("mysql:host=$servername;port=3306;dbname=2019BWebIntensive_tapfuma", $username, $password);
+   // set the PDO error mode to exception
+   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   echo "Connected successfully";
+   }
+catch(PDOException $e)
+   {
+   echo "Connection failed: " . $e->getMessage();
+   }
 ?>
